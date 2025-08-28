1eb41a4afade89a3ff32ae14856bbd0e3786c9a1
ellio <200138174+ElliotRey@users.noreply.github.com>
2025-08-27 20:49:28 -0600

    creando proyecto

---
+++ src/mx/uaemex/fi/paradigmas/numeros/Principal.java
@@ -0,0 +1,12 @@
+package mx.uaemex.fi.paradigmas.numeros;
+
+public class Principal {
+
+    public static void main(String [] args){
+        Racional r1;
+
+        r1 = new Racional();
+        System.out.println(r1);
+
+    }
+}
---
+++ src/mx/uaemex/fi/paradigmas/numeros/Racional.java
@@ -0,0 +1,40 @@
+package mx.uaemex.fi.paradigmas.numeros;
+
+/**
+ * data type
+ */
+public class Racional {
+    private int numerador;
+    private int denominador;
+
+    public Racional Potencia(int n){
+        Racional res;
+        int num, den;
+
+        num= (int) Math.pow(this.numerador,n);
+        den= (int) Math.pow(this.denominador,n);
+
+        res = new Racional(num,den);
+        return res;
+    }
+
+    public Racional(){
+        this.numerador = 0;
+        this.denominador = 1;
+    }
+
+    public Racional(Racional r){
+        this.numerador = r.numerador;
+        this.denominador = r.denominador;
+    }
+
+    public Racional(int numerador, int denominador){
+        this.numerador = numerador;
+        this.denominador = denominador;
+    }
+    
+    public String toString(){
+        return this.numerador + "/" + this.denominador;
+    }
+
+}
